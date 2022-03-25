b843223e1d9aa70c16cc7a29d094441e0abc5aec
marcustw <e0014884@u.nus.edu.sg>
2022-03-26 00:58:25 +0800

    fix: gitignore for every scraped_collection less mirlclub

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -42,7 +42,7 @@ def scrape_slug(slug: str):
   try:
     data = get_floor_prices(slug)
     print("Total {} NFTS scraped".format(len(data)))
-    scraper_utils.write_json_to_file('{}/scraped-collections/{}.json'.format(dir_path, slug), data)
+    scraper_utils.write_json_to_file('{}/scraped_collections/{}.json'.format(dir_path, slug), data)
     for obj in data:
       print(obj)
   except Exception as e:
